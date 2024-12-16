967b4ea87f33f1d2994ae3f03adda276c09f1dc3
suinming <0223314338aa@gmail.com>
2024-12-16 16:08:43 +0800

    feat: update wezterm setting to mimic tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,79 @@
 local wezterm = require("wezterm")
-
+local act = wezterm.action
 local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
 
 local config = {}
 
+config.font = wezterm.font("JetBrainsMono-Bold")
 config.font_size = 16
-config.enable_tab_bar = false
 config.colors = theme.colors()
+
+-- tiling window manager
+config.tiling_desktop_environments = {
+  'X11 i3',
+}
+
+-- print the workspace name at the upper right
+wezterm.on("update-right-status", function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+
+-- workspace 
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+workspace_switcher.zoxide_path = "/home/suinming/.local/bin/zoxide"
+config.leader = { key = ' ', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    mods   = "LEADER",
+    key    = "t",
+    action = workspace_switcher.switch_workspace()
+  },
+  {
+    mods   = "LEADER",
+    key    = "s",
+    action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" })
+  },
+}
+
+-- tab
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = false
+
+local function get_current_working_dir(tab)
+  local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+  local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+  return current_dir == HOME_DIR and "." or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local has_unseen_output = false
+  if not tab.is_active then
+    for _, pane in ipairs(tab.panes) do
+      if pane.has_unseen_output then
+        has_unseen_output = true
+        break
+      end
+    end
+  end
+
+  local cwd = wezterm.format({
+    { Attribute = { Intensity = "Bold" } },
+    { Text = get_current_working_dir(tab) },
+  })
+
+  local title = string.format(" [%s] %s ", tab.tab_index + 1, cwd)
+
+  if has_unseen_output then
+    return {
+      { Foreground = { Color = "#8866bb" } },
+      { Text = title },
+    }
+  end
+
+  return {
+    { Text = title },
+  }
+end)
 
 return config
