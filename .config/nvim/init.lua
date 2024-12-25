902b8baffabe71f6eb49e88008d0107c1577d544
suinming <0223314338aa@gmail.com>
2024-12-25 14:41:17 +0800

    feat: update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,46 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
+require("config.lazy")
 
-vim.opt.rtp:prepend(lazypath)
+vim.opt.shiftwidth = 2
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.swapfile = false
 
--- require("lazy").setup('plugins')
+-- source current file
+vim.keymap.set("n", "<space>x", "<cmd>source %<CR>")
+
+-- highlight when yanking (copying) text
+--  try it with `yap` in normal mode
+--  see `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- remove line number in nvim terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+-- open small nvim terminal at the bottom
+local job_id = 0
+vim.keymap.set("n", "<space>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J") -- put terminal at the bottom
+  vim.api.nvim_win_set_height(0, 5)
+  job_id = vim.bo.channel
+end)
+
+-- send command to nvim terminal
+vim.keymap.set("n", "<space>cmd", function()
+  -- example: send ls -al command
+  vim.fn.chansend(job_id, { "ls -al\r\n" })
+end)
