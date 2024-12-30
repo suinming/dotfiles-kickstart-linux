946fa6fc08d22cff1fed0d606b63648f3d96e0ff
suinming <0223314338aa@gmail.com>
2024-12-30 15:53:36 +0800

    feat: better pane navigation in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,6 +11,12 @@ vim.opt.swapfile = false
 -- source current file
 vim.keymap.set("n", "<space>x", "<cmd>source %<CR>")
 
+-- Navigate vim panes better
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
 -- highlight when yanking (copying) text
 --  try it with `yap` in normal mode
 --  see `:help vim.highlight.on_yank()`
